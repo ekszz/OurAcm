9010c619bcffc2ed1722f4c1ecd4cb6bad40c1ff
Ekszz <code@ekszz.com>
2013-12-09 22:12:51 +0800

    更改配置文件为WINDOWS换行格式

--- goldbirds/Conf/config.php
+++ goldbirds/Conf/config.php
@@ -4,7 +4,7 @@ return array(
     //Database
     'DB_TYPE'   		=> 	'mysql', 	    // 数据库类型
     'DB_HOST'   		=> 	'localhost',    // 服务器地址
-    'DB_NAME'   		=> 	'goldbirds_test',    // 数据库名
+    'DB_NAME'   		=> 	'ouracm',		// 数据库名
     'DB_USER'   		=> 	'root', 	    // 用户名
     'DB_PWD'    		=> 	'', 	        // 密码
     'DB_PORT'   		=> 	3306, 		    // 端口
