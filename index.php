6335ee509958d99fff3c4cfb84524a97ceb182fe
Ekszz <code@ekszz.com>
2014-01-08 10:50:40 +0800

    +首页增加新闻中心按钮

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     //定义项目路径
     define('APP_PATH', './goldbirds/');
     
-    define('APP_DEBUG', true);
+    define('APP_DEBUG', false);
     
     //加载框架入文件
     require './ThinkPHP/ThinkPHP.php';
