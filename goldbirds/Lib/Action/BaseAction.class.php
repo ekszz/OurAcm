5199f1feb66985d6c79bcb19da0984e57df42608
Ekszz <code@ekszz.com>
2014-07-06 15:08:23 +0800

    *更换缓存方法为S方法，解决F方法无法缓存空字符串的问题

--- goldbirds/Lib/Action/BaseAction.class.php
+++ goldbirds/Lib/Action/BaseAction.class.php
@@ -5,19 +5,19 @@ class BaseAction extends Action {
         $configDB = M('Setting');
         $data = $configDB -> field('k, v') -> select();
         foreach($data as $d) {
-            F($d['k'], $d['v']);
+            S($d['k'], $d['v']);  //不理解为啥TP缓存F方法要判断0长度的字符串无效?
         }
     }
     
     protected function getconfig($key) {  //获取某个参数
-        $data = F($key);
-        if($data === false) { $this -> init(); $data = $data = F($key); }  //增加一次容错重试
+        $data = S($key);
+        if($data === false) { $this -> init(); $data = $data = S($key); }  //增加一次容错重试
         if($data === false) return null;
         else return $data;
     }
     
     protected function setconfig($key, $value) {  //更改缓存中的参数值
-        F($key, $value);
+        S($key, $value);
     }
     
     protected function commonassign() {  //公共assign值
