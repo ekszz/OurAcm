fe5caa35a2c1da4d0446230a36be3d882c5ea8e6
Ekszz <code@ekszz.com>
2014-11-27 00:38:42 +0800

    *重写“码池”功能

--- Goldbirds/Home/Controller/CodepoolController.class.php
+++ Goldbirds/Home/Controller/CodepoolController.class.php
@@ -3,64 +3,128 @@ namespace Home\Controller;
 
 class CodepoolController extends BaseController {
     
-    public function index() {
+    private function safe_check($ip) {  //检测IP是否提交量过大
         
-        if(IS_GET) {  //查看代码
-            
-            $k = I('get.key');
-            $k = strtolower($k);
-            if(strlen($k) != 8 || !ctype_xdigit($k)) $this -> index_show(null);
-            else $this -> index_show($k);
+        $codepoolDB = M('Codepool');
+        $c['ip'] = array('EQ', $ip);
+        $count = $codepoolDB 
+            -> field('COUNT(codeid) AS c') 
+            -> where($c)
+            -> where('submittime < "'.date('Y-m-d H:i:s').'" AND submittime > "'.date('Y-m-d H:i:s', time() - 86400).'"')
+            -> select();
+        
+        if($count === false || intval($count[0]['c']) >= intval($this -> getconfig('codepool_maxperip'))) return false;  //提交量太大
+        else return true;  //正常
+    }
+    
+    private function k_check($key) {  //验证KEY是否合法，合法返回代码集
+        
+        $k = strtolower($key);
+        if(strlen($k) != 8 || !ctype_xdigit($k))  //KEY本身不合法
+            return false;
+        else {
+            $codepoolDB = M('Codepool');
+            $c['k'] = $key;
+            $res = $codepoolDB -> field('codeid, tag, code, ojaccount') -> where($c) -> order('codeid ASC') -> select();
+            if(!$res) return false;
+            else return $res;
         }
-        else if(IS_POST) {  //提交代码
-            
-            $k = I('get.key');
-            $k = strtolower($k);
-            if(strlen($k) != 8 || !ctype_xdigit($k)) $this -> index_show(null);
-            else {
-                $dat['k'] = $k;
-                $dat['exptime'] = date('Y-m-d H:i:s', time() + intval($this -> getconfig('codepool_exptime')));  //过期时间
-                $dat['tag'] = I('post.tag', '', false) == '' ? null : I('post.tag', false, '');
-                $dat['code'] = I('post.code', '', false);
-                $dat['ip'] = get_client_ip();
+    }
+    
+    public function index() {  //提交页面
 
-                $verify = new \Think\Verify();
-                if(!($verify -> check(I('post.verify', '', false)))) $this -> index_show($k, '[错误]验证码错误。');
-                else if(strlen($dat['code']) > intval($this -> getconfig('codepool_maxlength'))) $this -> index_show($k, '[错误]你提交的代码长度太长了。');
-                else if(!$dat['code']) $this -> index_show($k, '[错误]你提交的代码是空的-__-||');
-                else {
-                    $codepoolDB = M('Codepool');
-                    $codepoolDB -> add($dat);
-                    $this -> index_show($k);
-                }
+        $this -> commonassign();
+        $this -> display('index');
+    }
+    
+    public function ajax_submit() {  //提交代码
+        
+        $k = I('post.k');
+        if(!$k) {  //新提交
+            $codepoolDB = M('Codepool');
+            do {  //查询KEY是否已存在
+                $k = substr(md5('goldbirds_seed'.time().rand(1, 65536)), 2, 8);
+                $c['k'] = $k;
+            }while($codepoolDB -> field('k') -> where($c) -> find());
+            
+            $dat['k'] = $k;
+            $dat['submittime'] = date('Y-m-d H:i:s', time());
+            $dat['tag'] = I('post.tag', '', false) == '' ? null : I('post.tag', '', false);
+            $dat['code'] = I('post.code', '', false);
+            $dat['ip'] = get_client_ip();
+            $dat['ojaccount'] = ($this -> logincheck() == 0 ? null : session('goldbirds_oj'));
+            
+            $verify = new \Think\Verify();
+            if(!($verify -> check(I('post.verify', '', false)))) $this -> myajaxReturn(null, '[错误]验证码错误。', 1);
+            else if(strlen($dat['tag']) > 20) $this -> myajaxReturn(null, '[错误]你提交的标签长度太长了。最长20字节。', 2);
+            else if(!$this -> safe_check(get_client_ip())) $this -> myajaxReturn(null, '[错误]你今日提交的代码太多啦>.<', 4);
+            else if(strlen($dat['code']) > intval($this -> getconfig('codepool_maxlength'))) $this -> myajaxReturn(null, '[错误]你提交的代码长度太长了。', 2);
+            else if(!$dat['code']) $this -> myajaxReturn(null, '[错误]你提交的代码是空的-__-||', 3);
+            else {
+                $codepoolDB = M('Codepool');
+                $codepoolDB -> add($dat);
+                $this -> myajaxReturn('http://'.$_SERVER["HTTP_HOST"].$_SERVER["SCRIPT_NAME"].'?z=codepool-x-k-'.$c['k'], '[成功]', 0);
+            }
+        }
+        else {  //追加
+            $codepoolDB = M('Codepool');
+            //查询KEY是否已存在
+            $c['k'] = $k;
+            $ret = $this -> k_check($k);
+            if(!$ret) $this -> myajaxReturn(null, '[错误]无效的KEY。', 5);
+            
+            $dat['k'] = $k;
+            $dat['submittime'] = date('Y-m-d H:i:s', time());
+            $dat['tag'] = I('post.tag', '', false) == '' ? null : I('post.tag', '', false);
+            $dat['code'] = I('post.code', '', false);
+            $dat['ip'] = get_client_ip();
+            
+            if($this -> logincheck() == 0 || strcmp(session('goldbirds_oj'), $ret[0]['ojaccount']) != 0) $this -> myajaxReturn(null, '[错误]该代码不是你提交的。', 6);
+            $dat['ojaccount'] = session('goldbirds_oj');
+            
+            $verify = new \Think\Verify();
+            if(!($verify -> check(I('post.verify', '', false)))) $this -> myajaxReturn(null, '[错误]验证码错误。', 1);
+            else if(strlen($dat['tag']) > 20) $this -> myajaxReturn(null, '[错误]你提交的标签长度太长了。最长20字节。', 2);
+            else if(!$this -> safe_check(get_client_ip())) $this -> myajaxReturn(null, '[错误]你今日提交的代码太多啦>.<', 4);
+            else if(strlen($dat['code']) > intval($this -> getconfig('codepool_maxlength'))) $this -> myajaxReturn(null, '[错误]你提交的代码长度太长了。', 2);
+            else if(!$dat['code']) $this -> myajaxReturn(null, '[错误]你提交的代码是空的-__-||', 3);
+            else {
+                $codepoolDB = M('Codepool');
+                $id = $codepoolDB -> add($dat);
+                $topage['tag_id'] = 'c'.sprintf('%06d', $id);
+                $topage['tag'] = ($dat['tag'] == null ? '代码'.sprintf('%06d', $id) : htmlspecialchars($dat['tag']));
+                $topage['code'] = htmlspecialchars($dat['code']); 
+                $this -> myajaxReturn($topage, '[成功]', 0);
             }
         }
     }
     
-    private function index_show($k, $err = '') {  //传入的$k需保证合法，8位十六进制字符串
+    public function x() {  //提取代码
         
-        $data = array();
-        if($k) {
-            $c['k'] = $k;
-            $codepoolDB = M('Codepool');
-            $res = $codepoolDB -> where($c) -> order('codeid ASC') -> select();
-            
+        $k = I('get.k');
+        $res = $this -> k_check($k);
+        if(false === $res) {
+            $this -> assign('invalidkey', 'alert("[错误]该URL无效，你需要的资源可能已从地球消失了~~", "error");');
+            $this -> index();
+        }
+        else {
+            $data = array();
             foreach($res as $r) {
                 $data[$r['codeid']]['tag'] = ($r['tag'] == null ? '代码'.sprintf('%06d', $r['codeid']) : htmlspecialchars($r['tag']));
                 $data[$r['codeid']]['tag_id'] = 'c'.sprintf('%06d', $r['codeid']);
                 $data[$r['codeid']]['code'] = htmlspecialchars($r['code']);
             }
+            
+            $this -> assign('data', $data);
+            $this -> assign('url', 'http://'.$_SERVER["HTTP_HOST"].$_SERVER["SCRIPT_NAME"].'?z=codepool-x-k-'.$k);
+            $this -> assign('key', $k);
+            if($this -> logincheck() > 0 && strcmp($res[0]['ojaccount'], session('goldbirds_oj')) == 0) $this -> assign('issamelogin', 1);
+            else $this -> assign('issamelogin', 0);
+            $this -> commonassign();
+            $this -> display('x');
         }
-        
-        $this -> assign('data', $data);
-        if($data) $this -> assign('url', 'http://'.$_SERVER["HTTP_HOST"].$_SERVER["SCRIPT_NAME"].'?z=codepool-index-key-'.$c['k']);
-        if($data) $this -> assign('key', $c['k']);
-        else $this -> assign('key', substr(md5('goldbirds_seed'.time().rand(1, 65536)), 2, 8));
-        if($err) $this -> assign('err', $err);
-        $this -> commonassign();
-        $this -> display();
     }
-    
+
     public function verify() {  //生成验证码
         
         $config =    array(
@@ -78,7 +142,7 @@ class CodepoolController extends BaseController {
         
         if(strcmp(CODECLEANTOKEN, 'goldbirds') == 0) {
             $codepoolDB = M('Codepool');
-            $codepoolDB -> where('exptime < "'.date('Y-m-d H:i:s').'"') -> delete();
+            $codepoolDB -> where('submittime < "'.date('Y-m-d H:i:s', time() - intval($this -> getconfig('codepool_exptime'))).'"') -> delete();
         }
     }
 }
--- db/goldbirds_upgrade_0.5.0_to_0.6.0.sql
+++ db/goldbirds_upgrade_0.5.0_to_0.6.0.sql
@@ -4,9 +4,10 @@ SET time_zone = "+00:00";
 CREATE TABLE IF NOT EXISTS `codepool` (
   `codeid` int(11) NOT NULL AUTO_INCREMENT COMMENT '代码ID',
   `k` varchar(8) NOT NULL COMMENT 'key键',
-  `exptime` datetime NOT NULL COMMENT '过期时间',
-  `tag` varchar(128) DEFAULT NULL COMMENT '左侧标签',
+  `submittime` datetime NOT NULL COMMENT '提交时间',
+  `tag` varchar(32) DEFAULT NULL COMMENT '左侧标签',
   `code` text NOT NULL COMMENT '代码内容',
+  `ojaccount` varchar(32) DEFAULT NULL COMMENT '提交时的OJ账号', 
   `ip` varchar(32) DEFAULT NULL COMMENT '客户端IP',
   PRIMARY KEY (`codeid`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COMMENT='码池表' AUTO_INCREMENT=1 ;
@@ -15,5 +16,7 @@ INSERT INTO `setting` (`k`, `name`, `v`, `desc`, `type`) VALUES
 ('codepool_exptime', '码池-代码保留时间', '2592000', '码池功能中用户提交的代码保留的时间，单位为秒。注意：修改此参数不会改变已提交的代码的保留时间。', 2);
 INSERT INTO `setting` (`k`, `name`, `v`, `desc`, `type`) VALUES
 ('codepool_maxlength', '码池-单个代码最大长度', '32768', '码池功能中单个代码允许提交的最大长度，单位字节。', '2');
+INSERT INTO `setting` (`k`, `name`, `v`, `desc`, `type`) VALUES 
+('codepool_maxperip', '码池-单IP每24小时最大提交量', '200', '码池功能中，每个IP地址24小时内最大提交的代码数量。该值用予防止机器人提交。', '2');
 
 ALTER TABLE `contest` CHANGE `medal` `medal` INT(2) NOT NULL COMMENT '奖牌，0-金牌，1-银牌，2-铜牌，3-鼓励奖，4-旅游队'; 
