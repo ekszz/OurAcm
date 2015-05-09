7f5001942abd567720a0fff01674344752303095
Ekszz <code@ekszz.com>
2015-05-09 17:03:05 +0800

    +增加3个OJ接口 +活动模块中，未登录时，增加提示注册地址

--- www/Goldbirds/Home/Common/function.php
+++ www/Goldbirds/Home/Common/function.php
@@ -17,6 +17,18 @@ class OJLoginInterface {  //OJ登录接口，请根据自己的OJ修改
     static public function getLoginURL() {  //返回OJ登录地址
         return '/fasast/login.php';
     }
+    
+    static public function getRegURL() {  //返回OJ注册地址
+        return '/fasast/register.php';
+    }
+    
+    static public function getProblemURL($pid) {  //返回题目ID为$pid的对应题目URL，返回null则禁用相关功能
+        return '/fasast/problem.php?pid='.$pid;
+    }
+    
+    static public function getUserURL($uname) {  //返回用户名为$uname的详细信息，返回null则禁用相关功能
+        return 'fasast/user.php?uname='.$uname;
+    }
 
 	static public function getOJURL() {  //返回OJ主页地址
         return '/fasast/';
