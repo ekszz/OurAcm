5ceb53dd665e39d5e84dbe08d1aaead5caa5dffd
Ekszz <code@ekszz.com>
2015-05-13 22:30:20 +0800

    *解决变态IE浏览器中上方提示框不居中的问题

--- www/js/setting_common.js
+++ www/js/setting_common.js
@@ -13,6 +13,7 @@ $(function() {
 function alert(msg, t){
 	if(t == 'error') { $('#alert').html("<div class='center alert alert-danger message fade in'><a class='dismiss close' data-dismiss='alert'>x</a>" + msg + "</div>"); }
 	else { $('#alert').html("<div class='center alert alert-warning message fade in'><a class='dismiss close' data-dismiss='alert'>x</a>" + msg + "</div>"); }
+	$("#alert .alert").css("left", ($(window).width()-$("#alert .alert").css("width").substr(0, $("#alert .alert").css("width").length-2))/2);
     $("#alert .alert").show();
     $("#alert .alert").delay(5000).fadeIn(6000).fadeOut(1000);
 }
